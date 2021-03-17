83ae98fae7d40437116b8381eecf8c305da01678
Sean (Lumos) <seanstapleton100@gmail.com>
2021-03-16 19:40:27 -0700

    add exclamation to hello world

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1>Hello, World!</h1>
+        <h1>Hello, World!!</h1>
       </main>
 
       <footer className={styles.footer}>
